772ebb685372371e452be2544d8b3689a2dad0c7
M.I. Afker Ahmed <imafkerahmed@gmail.com>
2026-02-11 23:09:26 +0530

    initited the admin course view

--- src/app/dashboard/admin/intakes/[intakeId]/page.tsx
+++ src/app/dashboard/admin/intakes/[intakeId]/page.tsx
@@ -363,7 +363,12 @@ export default function IntakeDetailsPage() {
                   ci.courseDetails ? (
                     <tr
                       key={ci.id}
-                      className={idx % 2 === 0 ? "bg-white" : "bg-gray-50"}
+                      className={`${idx % 2 === 0 ? "bg-white" : "bg-gray-50"} cursor-pointer hover:bg-indigo-50 transition`}
+                      onClick={() =>
+                        router.push(
+                          `/dashboard/admin/intakes/${intakeId}/${ci.courseDetails?.id}`,
+                        )
+                      }
                     >
                       <td className="px-6 py-4 whitespace-nowrap font-medium text-gray-900">
                         {ci.courseDetails.name}
